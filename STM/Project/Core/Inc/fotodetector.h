58aef876e57500ec79fccbe32b7540b29f475850
eryk2303 <37594103+eryk2303@users.noreply.github.com>
2020-03-10 20:57:54 +0100

    2

---
+++ STM/Project/Core/Inc/fotodetector.h
@@ -0,0 +1,8 @@
+#include "main.h"
+#include "adc.h"
+#include "usart.h"
+
+uint8_t fotodetector_value;
+
+
+uint8_t get_value_fotodetector();
---
+++ STM/Project/Core/Src/fotodetector.c
@@ -0,0 +1,12 @@
+#include "fotodetector.h"
+
+
+uint8_t get_value_fotodetector()
+{
+		HAL_ADC_Start(&hadc);
+		HAL_ADC_PollForConversion(&hadc, HAL_MAX_DELAY);
+		fotodetector_value = HAL_ADC_GetValue(&hadc);
+
+		 return fotodetector_value;
+
+}
